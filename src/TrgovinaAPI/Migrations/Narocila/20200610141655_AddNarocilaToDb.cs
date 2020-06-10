b77dd4d509fb8efcdeaba54b97970eace37ad2cc
lizba <blaz.baliz@gmail.com>
2020-06-10 16:18:11 +0200

    Added Narocila to DB

---
+++ src/TrgovinaAPI/Migrations/Narocila/20200610141655_AddNarocilaToDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace TrgovinaAPI.Migrations.Narocila
+{
+    public partial class AddNarocilaToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Narocila",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UporabnikId = table.Column<int>(nullable: false),
+                    IzdelekId = table.Column<int>(nullable: false),
+                    Kolicina = table.Column<int>(nullable: false),
+                    Status = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Narocila", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Narocila");
+        }
+    }
+}
